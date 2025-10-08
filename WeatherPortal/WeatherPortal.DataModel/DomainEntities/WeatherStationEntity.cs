3b446701008b51a6481e17015c69cd8b4f4d45a9
Khaing Zin Htet <khaingzinhtet562000@gmail.com>
2025-10-08 21:34:52 +0630

    Add WeatherStationEntity
    Add WeatherStationEntity

---
+++ WeatherPortal/WeatherPortal.DataModel/DomainEntities/WeatherStationEntity.cs
@@ -0,0 +1,10 @@
+﻿using WeatherPortal.DataModel.BaseEntities;
+
+namespace WeatherPortal.DataModel.DomainEntities
+{
+    public class WeatherStationEntity:BaseEntity
+    {
+        public string StationName { get; set; }
+        public string TownshipId { get; set; }
+    }
+}
